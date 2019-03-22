1fe39db9511b69234ecec06df9505150d56bd452
larasfitrianingsih <larasfitriani26@gmail.com>
2019-03-22 10:19:43 +0700

    laras

--- laras/routes/web.php
+++ laras/routes/web.php
@@ -15,15 +15,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/helo', function () {
-    return ('Hello Worasasa!!');
+Route::get('helo', function () {
+    return ('Hello World!!');
 });
 
 Route::get('/welcome', function () {
-echo "Welcome";
-}); 
+    echo "welcome";
+});
 
+
+Route::post('/store', function () {
+// sintak untuk menyimpan data
+});
 
 Route::get('/show/{id}', function ($id) {
 echo "Nilai Parameter Adalah ".$id;
 });
+
+Route::get('/edit/{nama}', function ($nama) {
+echo "Nilai Parameter Adalah ".$nama;
+})->where('nama','[A-Za-z]+');
+
+Route::get('/index', function () {
+echo "<a href='".route('create')."'>Akses Route dengan nama </a>";
+});
+
+Route::get('/create', function () {
+echo "Route diakses menggunakan nama";
+})->name('create');
+
+Route::get('/barangs', 'barangsController@index');
